b72ccf6eb4162d3b6aca8c47f13ddb0607dcecca
MuhammadRezaldy <rezaldymm1@gmail.com>
2022-12-15 15:03:07 +0800

    instance, buat attribut dan panggil method

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package praktek03;
 
-/**
- *
- * @author win 10
- */
+
 public class SepedaAksi {
-    
+    public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-Kupu";
+        s.warna="Merah";
+        s.harga=1000;
+        
+        s.cetakInfo();
+    }
 }
