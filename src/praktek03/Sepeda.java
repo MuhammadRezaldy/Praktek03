775eaa2ecbe963be69ecb3a76b5a6bd94c88dbb6
MuhammadRezaldy <rezaldymm1@gmail.com>
2022-12-15 15:09:41 +0800

    set attribut private 2

--- src/praktek03/Sepeda.java
+++ src/praktek03/Sepeda.java
@@ -3,13 +3,39 @@ package praktek03;
 
 
 public class Sepeda {
-    Private merk;
-    Private warna;
-    Private harga;
+    private String merk;
+    private String warna;
+    private double harga;
     
     void cetakInfo(){
         System.out.println("merk \t: "+merk+"\n"+
                 "warna \t: "+warna+"\n"+
                 "harga \t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
